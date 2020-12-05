b0fe9064248a21e68c0194811e88655ab0e7177b
Maya Messinger <maya.messinger@gmail.com>
2020-12-04 18:49:39 -0600

    day 4 - working part 2

--- challenges/day4_script.js
+++ challenges/day4_script.js
@@ -28,13 +28,71 @@ class Passport {
 	}
 
 	isValid() {
-		return this.byr !== null
-			&& this.iyr !== null
-			&& this.eyr !== null
-			&& this.hgt !== null
-			&& this.hcl !== null
-			&& this.ecl !== null
-			&& this.pid !== null;
+		return Passport.birthYearValid(this.byr)
+			&& Passport.issueYearValid(this.iyr)
+			&& Passport.expYearValid(this.eyr)
+			&& Passport.heightValid(this.hgt)
+			&& Passport.hairColorValid(this.hcl)
+			&& Passport.eyeColorValid(this.ecl)
+			&& Passport.passportIdValid(this.pid);
+	}
+
+	static birthYearValid(year) {
+		return year !== null
+			&& year.length === 4
+			&& parseInt(year) >= 1920
+			&& parseInt(year) <= 2002;
+	}
+
+	static issueYearValid(year) {
+		return year !== null
+			&& year.length === 4
+			&& parseInt(year) >= 2010
+			&& parseInt(year) <= 2020;
+	}
+
+	static expYearValid(year) {
+		return year !== null
+			&& year.length === 4
+			&& parseInt(year) >= 2020
+			&& parseInt(year) <= 2030;
+	}
+
+	static heightValid(height) {
+		var formatRegex = /^([0-9]+)(cm|in)$/;
+
+		var exec = formatRegex.exec(height);
+		if (exec !== null) {
+			var num = parseInt(exec[1]);
+
+			if (exec[2] === "cm")
+				return num >= 150 && num <= 193;
+			else if (exec[2] === "in")
+				return num >= 59 && num <= 76;
+		}
+
+		return false;
+	}
+
+	static hairColorValid(color) {
+		var regex = /^#[0-9a-f]{6}$/;
+
+		return color !== null
+			&& regex.exec(color) !== null;
+	}
+
+	static eyeColorValid(color) {
+		const validColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+
+		return color !== null
+			&& validColors.indexOf(color) !== -1;
+	}
+
+	static passportIdValid(pid) {
+		var regex = /^[0-9]{9}$/;
+
+		return pid !== null
+			&& regex.exec(pid) !== null;
 	}
 }
 
