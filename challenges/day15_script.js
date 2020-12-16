0544aa2ee2d95caecb280cf86f4a5831d2e5c09f
Maya Messinger <maya.messinger@gmail.com>
2020-12-15 19:36:44 -0600

    day 15 - working part 2

--- challenges/day15_script.js
+++ challenges/day15_script.js
@@ -4,7 +4,7 @@ const fileName = '../inputs/day15_input.txt';
 
 function run() {
 	const startingNumbers = populateStartingNumbers();
-	const goalNumber = runGame(startingNumbers, 2020);
+	const goalNumber = runGame(startingNumbers, 30000000);
 
 	console.log(goalNumber);
 }
@@ -20,23 +20,46 @@ function populateStartingNumbers() {
 	return startingNumbers;
 }
 
+/**
+ * numberInfo tracks a number, and the last 2 times it has been said.
+ * any time a number is said, update the info about it in numberInfo
+ * From there, we can check:
+ * a.) if a number has been said before the most recent round
+ * b.) the difference between the most recent speakings of a number
+ */
 function runGame(startingNumbers, numRounds) {
-	let reversedNumbers = [];
+	let numberInfo = new Map();
 
 	for (var i = 0; i < startingNumbers.length; i++) {
-		reversedNumbers.unshift(startingNumbers[i]);
+		numberInfo.set(startingNumbers[i], [i, undefined]);
 	}
 
-	for (var i = 0; i < numRounds - startingNumbers.length; i++) {
-		const age = reversedNumbers.indexOf(reversedNumbers[0], 1);
+	let lastNumber = startingNumbers[startingNumbers.length - 1];
+	for (var i = startingNumbers.length; i < numRounds; i++) {
+		const age = numberInfo.get(lastNumber);
 
-		if (age === -1)
-			reversedNumbers.unshift(0);
-		else
-			reversedNumbers.unshift(age);
+		if (age === undefined || age[1] === undefined) {
+			const zeroInfo = numberInfo.get(0);
+
+			numberInfo.set(0, [i, zeroInfo[0]]);
+			lastNumber = 0;
+		}
+		else {
+			const diff = age[0] - age[1];
+			const diffInfo = numberInfo.get(diff);
+
+			if (diffInfo === undefined)
+				numberInfo.set(diff, [i, undefined]);
+			
+			else
+				numberInfo.set(diff, [i, diffInfo[0]]);
+
+			lastNumber = diff;
+
+		}
 	}
 
-	return reversedNumbers[0];
+	return lastNumber;
 }
 
 module.exports.run = run;
