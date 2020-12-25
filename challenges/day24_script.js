0ce9375ef8202c1858c818ba5fc282f961a25ab7
Maya Messinger <maya.messinger@gmail.com>
2020-12-24 23:36:07 -0600

    day 24 - working part 2

--- challenges/day24_script.js
+++ challenges/day24_script.js
@@ -15,7 +15,8 @@ class Tile {
 }
 
 function run() {
-	const tiles = parseInput();
+	let tiles = parseInput();
+	tiles = flipDaily(tiles, 100);
 	const numBlack = getBlackTiles(tiles);
 
 	console.log(numBlack);
@@ -89,6 +90,71 @@ function parsePath(path) {
 
 function getKey(tuple) {
 	return tuple[0] + "." + tuple[1];
+}
+
+function flipDaily(tiles, numDays) {
+	for (var i = 1; i <= numDays; i++) {
+
+		const newDayTiles = new Map();
+
+		// pretty bad to hard-code floor bounds, but I figured it's actually more efficient
+		// than finding array max coordinates every day. It did take trial and error,
+		// have to increase bounds until final answer does not change
+		for (var j = -65; j <= 65; j++) {
+			for (var k = -65; k <= 65; k++) {
+				const tile = tiles.get(getKey([j, k])) ?? new Tile(j, k);
+				const blackNeighbors = getBlackNeighbors(tiles, tile);
+				const willBeBlack = wilLBeBlack(tile, blackNeighbors);
+
+				newDayTiles.set(getKey([j, k]), new Tile(tile.x, tile.y, willBeBlack));
+			}
+		}
+
+		tiles = newDayTiles;
+	}
+
+	return tiles;
+}
+
+function getBlackNeighbors(tiles, tile) {
+	const neighborCoords = getNeighborCoords(tile);
+
+	let blackNeighbors = 0;
+	neighborCoords.forEach(c => {
+		if (tiles.get(getKey(c)) !== undefined && tiles.get(getKey(c)).isBlack)
+			blackNeighbors++;
+	});
+
+	return blackNeighbors;
+}
+
+function getNeighborCoords(tile) {
+	return tile.y % 2 === 0
+		? [
+			[tile.x + 1, tile.y],
+			[tile.x, tile.y - 1],
+			[tile.x - 1, tile.y - 1],
+			[tile.x - 1, tile.y],
+			[tile.x - 1, tile.y + 1],
+			[tile.x, tile.y + 1]
+		]
+		: [
+			[tile.x + 1, tile.y],
+			[tile.x + 1, tile.y - 1],
+			[tile.x, tile.y - 1],
+			[tile.x - 1, tile.y],
+			[tile.x, tile.y + 1],
+			[tile.x + 1, tile.y + 1]
+		];
+}
+
+function wilLBeBlack(tile, blackNeighbors) {
+	if (tile.isBlack && blackNeighbors === 0 || blackNeighbors > 2)
+		return false;
+	if (!tile.isBlack && blackNeighbors === 2)
+		return true;
+
+	return tile.isBlack;
 }
 
 function getBlackTiles(tiles) {
